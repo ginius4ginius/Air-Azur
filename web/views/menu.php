f565a4181ff3c9c41d3a2a50892f1df375cb8aec
ginius4ginius <derieu.vincent@outlook.com>
2017-12-15 13:49:31 +0100

    commit

--- web/views/menu.php
+++ web/views/menu.php
@@ -2,9 +2,9 @@
     <div class="col-lg-12">
         <nav class="navbar navbar-default">
           <ul class="nav navbar-nav">
-              <li class="active"> <a href="home.php"><span class="glyphicon glyphicon-home">   Accueil </span></a> </li>
-              <li> <a href="#"><span class="glyphicon glyphicon-plane"></span>    Liste des vols</a> </li>
-              <li> <a href="#"><span class="glyphicon glyphicon-edit"></span>   Réservation</a> </li>
+              <li class="active"> <a href="?action=home"><span class="glyphicon glyphicon-home">   Accueil </span></a> </li>
+              <li> <a href="?action=catalog"><span class="glyphicon glyphicon-plane"></span>    Liste des vols</a> </li>
+              <li> <a href="?action=reservation"><span class="glyphicon glyphicon-edit"></span>   Réservation</a> </li>
           </ul>
       <button type="button" class="btn btn-default navbar-btn">Deconnexion</button>
       </nav>
