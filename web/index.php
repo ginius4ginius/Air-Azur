8090717cfd98066072c0050acdb25e8ab3bb8570
Frederic Vermont <f.vermont@wanadoo.fr>
2017-12-15 11:38:32 +0100

    index skeletton

--- web/index.php
+++ web/index.php
@@ -1,15 +1,21 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <?php include("head.php"); ?>
-    <body>
-        <?php
-      
-        ?>
-    </body>
-  <?php include("foot.php"); ?>
-</html>
+<?php
+  if(isset($_REQUEST['action'])) {
+    switch($_REQUEST['action']) {
+      case 'reservation':
+        header("Location: views/reservation.php");
+        //
+        echo "Hello reservation";
+        break;
+      //
+      case 'catalog':
+        //header("Location: views/reservation.php");
+        echo "Hello catalog";
+        break;
+      //
+    }
+
+  }
+  //
+  echo "Hello word!!!!";
+
+ ?>
