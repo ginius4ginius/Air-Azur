83a548de14cfa33a3280b0a820e8b50d29664ee9
fvermont <f.vermont@wanadoo.fr>
2018-01-04 17:57:48 +0100

    Simplification index

--- web/index.php
+++ web/index.php
@@ -1,42 +1,22 @@
-<html>
-<?php include_once('views/head.php') ?>
- <body>
-   <?php include_once('views/header.php') ?>
-
-     <div id="menu" class="row">
-         <div class="col-lg-12">
-           <div class="col-lg-3">
-           </div>
-              <div class="col-lg-6">
-   <?php
-  // --- router
-  // --- routes to controller depending on action
-  //
-
-  if(isset($_REQUEST['action'])) {
-    switch($_REQUEST['action']) {
-      case 'reservation':
-        header("Location: controller/reservation.php");
-        break;
-      //
-      case 'catalog':
-        header("Location: controller/catalog.php");
-        break;
-        //
-    }
-
+<?php
+//
+$sLocation = "views/home.php"; //default location route
+//
+//--------------------------------------- route
+if(isset($_REQUEST['action'])) {
+  switch($_REQUEST['action']) {
+    case 'reservation':
+      $sLocation = "controller/reservation.php";
+      break;
+    //
+    case 'catalog': 
+      $sLocation = "controller/catalog.php";
+      break;
+    //
   }
-  //
-  header("Location: views/home.php");
-  //
+}
+//----------- go to location
+header("Location: $sLocation");
+//exit();
+//
  ?>
-          </div>
-        <div class="col-lg-3">
-        </div>
-      </div>
-    </div>
- <?php include_once('views/foot.php') ?>
- <script src="http://code.jquery.com/jquery.js"></script>
- <script src="css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-  </body>
-</html>
