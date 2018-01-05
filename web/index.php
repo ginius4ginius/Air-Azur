a1cbfaf6cd54d5df97fb742183090227db14852f
ginius4ginius <derieu.vincent@outlook.com>
2018-01-05 13:58:19 +0100

    commit

--- web/index.php
+++ web/index.php
@@ -2,16 +2,8 @@
 <html>
 <?php include_once('views/head.php') ?>
  <body>
-   <?php include_once('views/header.php') ?>
+   <?php include_once('views/header.php')
 
-      <!--
-      <div id="menu" class="row">
-         <div class="col-lg-12">
-           <div class="col-lg-3">
-           </div>
-              <div class="col-lg-6">
-      -->
-   <?php
   // --- router
   // --- routes to controller depending on action
   //
@@ -27,10 +19,13 @@
         break;
         //
     }
-
+  }
+/*
 =======
 <?php
 //
+*/
+
 $sLocation = "views/home.php"; //default location route
 //
 //--------------------------------------- route
@@ -40,11 +35,13 @@ if(isset($_REQUEST['action'])) {
       $sLocation = "controller/reservation.php";
       break;
     //
-    case 'catalog': 
+    case 'catalog':
       $sLocation = "controller/catalog.php";
       break;
     //
+    /*
 >>>>>>> 445552315796288620fefd25293870c7749fabd3
+*/
   }
 }
 //----------- go to location
@@ -52,12 +49,15 @@ header("Location: $sLocation");
 //exit();
 //
  ?>
+ <!--
 <<<<<<< HEAD
-
+-->
  <?php include_once('views/foot.php') ?>
  <script src="http://code.jquery.com/jquery.js"></script>
  <script src="css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
   </body>
 </html>
+<!--
 =======
 >>>>>>> 445552315796288620fefd25293870c7749fabd3
+-->
