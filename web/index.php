e0593f07946469e17217fb10c8683907bfce599b
fvermont <f.vermont@wanadoo.fr>
2018-01-05 11:50:33 +0100

    Fix- mauvaise manip

--- web/index.php
+++ web/index.php
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-<html>
-<?php include_once('views/head.php') ?>
- <body>
-   <?php include_once('views/header.php') ?>
-
-      <!--
-      <div id="menu" class="row">
-         <div class="col-lg-12">
-           <div class="col-lg-3">
-           </div>
-              <div class="col-lg-6">
-      -->
-   <?php
-  // --- router
-  // --- routes to controller depending on action
-  //
-
-  if(isset($_REQUEST['action'])) {
-    switch($_REQUEST['action']) {
-      case 'reservation':
-        header("Location: controller/reservation.php");
-        break;
-      //
-      case 'catalog':
-        header("Location: controller/catalog.php");
-        break;
-        //
-    }
-
-=======
 <?php
 //
 $sLocation = "views/home.php"; //default location route
@@ -44,7 +13,6 @@ if(isset($_REQUEST['action'])) {
       $sLocation = "controller/catalog.php";
       break;
     //
->>>>>>> 445552315796288620fefd25293870c7749fabd3
   }
 }
 //----------- go to location
@@ -52,12 +20,3 @@ header("Location: $sLocation");
 //exit();
 //
  ?>
-<<<<<<< HEAD
-
- <?php include_once('views/foot.php') ?>
- <script src="http://code.jquery.com/jquery.js"></script>
- <script src="css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-  </body>
-</html>
-=======
->>>>>>> 445552315796288620fefd25293870c7749fabd3
