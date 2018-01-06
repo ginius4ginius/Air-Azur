c56726f9df3185600d1416807a850d67e69b23f7
ginius4ginius <derieu.vincent@outlook.com>
2018-01-06 06:47:06 +0100

    petit discour dans l'accueil.

--- web/views/home.php
+++ web/views/home.php
@@ -5,7 +5,17 @@
       <?php include_once('header.php')?>
       <div>
         <div id="accueil">
-          Bienvenue sur l'intranet d'AIR AZUR
+          Bienvenue sur l'intranet d'AIR AZUR <br />
+          <br />
+          <br />
+          À propos d’Air Azur <br />
+
+Air Azur est l'un des plus importants transporteur aérien Français spécialisé dans les voyages vacances. Chaque année, la société transporte environ 3 millions de passagers vers près de 3 destinations dans 3 pays à l’aide d’une flotte de gros porteurs Airbus. La société emploie environ 2 personnes. Air Azur est une filiale de CDI A.A. inc., un voyagiste international intégré qui compte plus de 2 pays de destination et qui distribue des produits dans plus de 3 pays. Air Azur a été nommée meilleure ligne aérienne vacances en Amérique du Nord lors de la remise de prix annuelle des World Airline Awards organisée par Skytrax en juin 2015.
+<br />
+CDI A.A. inc. est un voyagiste international intégré qui compte plus de 2 pays de destination et qui distribue des produits dans plus de 3 pays. Spécialiste du voyage vacances, Transat est principalement active en France. CDI, dont le siège social est situé à Cachan, est aussi présente dans le transport aérien, l’hôtellerie.
+<br />
+Chez CDI, nous reconnaissons que l'environnement, les collectivités qui accueillent les voyageurs, la diversité culturelle et nos relations avec nos employés, clients et partenaires sont d'une importance capitale. Voilà pourquoi CDI a adopté une politique de responsabilité d’entreprise en 2008. De plus, Air Azur s’engage à réduire son empreinte écologique et a adopté une politique environnementale.
+
         </div>
       </div>
     </div>
@@ -16,5 +26,5 @@
       console.log( "home and ready!" );
       $( "#menu_home" ).addClass('active');
     });
-  </script>  
+  </script>
 </html>
